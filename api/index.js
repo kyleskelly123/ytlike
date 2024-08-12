3bad62e3e92823b56702636436c73c9f3c56af40
“Kyle <Kyle@kyleskelly.co.uk>
2024-08-12 19:54:12 +0100

    changes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+// api/index.js
 const serverless = require('serverless-http');
-const axios = require('axios');
-const path = require('path');
+const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports.handler = serverless(app);
 
 // Access environment variables
 const PERSONAL_ACCESS_TOKEN = process.env.PERSONAL_ACCESS_TOKEN;
